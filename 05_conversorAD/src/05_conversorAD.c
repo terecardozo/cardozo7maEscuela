85f9be2adbb4077dc5acb6b168d8a25af4873fc3
terecardozo <terecardozo@gmail.com>
2016-11-03 10:08:57 -0300

    Aplicacion que de acuerdo al pasar por rangos especificos de tension encienden leds

---
+++ 05_conversorAD/src/05_conversorAD.c
@@ -0,0 +1,148 @@
+/* Copyright 2016, XXXX
+ *
+ *  * All rights reserved.
+ *
+ * This file is part of CIAA Firmware.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+/** \brief Blinking Bare Metal example source file
+ **
+ ** This is a mini example of the CIAA Firmware.
+ **
+ **/
+
+/** \addtogroup CIAA_Firmware CIAA Firmware
+ ** @{ */
+
+/** \addtogroup Examples CIAA Firmware Examples
+ ** @{ */
+/** \addtogroup Baremetal Bare Metal example source file
+ ** @{ */
+
+/*
+ * Initials     Name
+ * ---------------------------
+ *
+ */
+
+/*
+ * modification history (new versions first)
+ * -----------------------------------------------------------
+ * yyyymmdd v0.0.1 initials initial version
+ */
+
+/*==================[inclusions]=============================================*/
+#include "05_conversorAD.h"       /* <= own header */
+#define valor_maximo 1020
+#define valor_minimo 5
+#define CONST_TIMER 3000000
+
+
+/*==================[macros and definitions]=================================*/
+void Delay(void){
+	uint64_t i;
+
+ for(i=CONST_TIMER; i!=0; i--);
+
+}
+
+void Convertir_senial(){
+	uint16_t valor;
+
+	valor = read_ADC_value_pooling();
+	    	if(valor > valor_maximo)
+	    	{
+	    		TooggleLed(LED_3);
+	    		Delay();
+	    		//EncenderLed(LED_3);
+	    		ApagarLed(LED_1);
+	    	}
+	    	else
+	    	{
+				if(valor<valor_minimo)
+				{
+					EncenderLed(LED_1);
+					ApagarLed(LED_3);
+				}
+				else
+				{
+					ApagarLed(LED_1);
+					ApagarLed(LED_3);
+				}
+	    	}
+}
+/*==================[internal data declaration]==============================*/
+
+/*==================[internal functions declaration]=========================*/
+
+/*==================[internal data definition]===============================*/
+
+/*==================[external data definition]===============================*/
+
+/*==================[internal functions definition]==========================*/
+
+/*==================[external functions definition]==========================*/
+/** \brief Main function
+ *
+ * This is the main entry point of the software.
+ *
+ * \returns 0
+ *
+ * \remarks This function never returns. Return value is only to avoid compiler
+ *          warnings or errors.
+ */
+
+
+
+int main(void)
+{
+   /* inicializaciones */
+
+    InitLeds();
+	init_ADC_EDUCIAA();
+   
+   
+   /*acá va mi programa principal */
+   
+   ADC_Sel(CH1);
+    while(1){
+    	Convertir_senial();
+    };
+    
+	return 0;
+
+
+}
+
+/** @} doxygen end group definition */
+/** @} doxygen end group definition */
+/** @} doxygen end group definition */
+/*==================[end of file]============================================*/
+
